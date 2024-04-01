cc9d6b41d1fcd80c56deed8f7d1adadb6b9f7d0e
Fireball_2000 <kerlan.jacquette@gmail.com>
2024-04-01 09:15:42 -0400

    capybaras can be tamed

--- src/main/java/sweet/wilderness/entity/entities/capybara/CapybaraEntity.java
+++ src/main/java/sweet/wilderness/entity/entities/capybara/CapybaraEntity.java
@@ -1,14 +1,19 @@
 package sweet.wilderness.entity.entities.capybara;
 
 import net.minecraft.entity.EntityType;
-import net.minecraft.entity.ai.goal.LookAroundGoal;
-import net.minecraft.entity.ai.goal.SwimGoal;
-import net.minecraft.entity.ai.goal.WanderAroundFarGoal;
+import net.minecraft.entity.ai.goal.*;
 import net.minecraft.entity.attribute.DefaultAttributeContainer;
 import net.minecraft.entity.attribute.EntityAttributes;
 import net.minecraft.entity.passive.AnimalEntity;
 import net.minecraft.entity.passive.PassiveEntity;
+import net.minecraft.entity.passive.TameableEntity;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.Items;
 import net.minecraft.server.world.ServerWorld;
+import net.minecraft.util.ActionResult;
+import net.minecraft.util.Hand;
+import net.minecraft.world.EntityView;
 import net.minecraft.world.World;
 import org.jetbrains.annotations.Nullable;
 import software.bernie.geckolib.animatable.GeoEntity;
@@ -18,14 +23,15 @@ import software.bernie.geckolib.core.animation.*;
 import software.bernie.geckolib.core.object.PlayState;
 import software.bernie.geckolib.util.GeckoLibUtil;
 import sweet.wilderness.entity.ModEntities;
+import sweet.wilderness.huh.EntityUtils;
 
-public class CapybaraEntity extends AnimalEntity implements GeoEntity {
+public class CapybaraEntity extends TameableEntity implements GeoEntity {
     private AnimatableInstanceCache cache = GeckoLibUtil.createInstanceCache(this);
 
     public static final float MOVEMENT_SPEED = .5201f;
 
     public CapybaraEntity(EntityType<? extends AnimalEntity> entityType, World world) {
-        super(entityType, world);
+        super((EntityType<? extends TameableEntity>) entityType, world);
     }
 
     public static DefaultAttributeContainer.Builder setAttributes() {
@@ -37,8 +43,11 @@ public class CapybaraEntity extends AnimalEntity implements GeoEntity {
     @Override
     protected void initGoals() {
         this.goalSelector.add(1, new SwimGoal(this));
-        this.goalSelector.add(2, new WanderAroundFarGoal(this, 0.75f, 1));
-        this.goalSelector.add(3, new LookAroundGoal(this));
+        this.goalSelector.add(2, new FollowOwnerGoal(this, 1.0, 10.0F, 2.0F, false));
+        this.goalSelector.add(3, new AnimalMateGoal(this, 1.0));
+        this.goalSelector.add(4, new WanderAroundFarGoal(this, 0.75f, 1));
+        this.goalSelector.add(5, new LookAroundGoal(this));
+        this.goalSelector.add(6, new LookAtEntityGoal(this, PlayerEntity.class, 8.0F));
     }
 
     @Nullable
@@ -73,5 +82,23 @@ public class CapybaraEntity extends AnimalEntity implements GeoEntity {
     @Override
     public AnimatableInstanceCache getAnimatableInstanceCache() {
         return cache;
+    }
+
+
+    @Override
+    public EntityView method_48926() {
+        return this.getWorld();
+    }
+
+    @Override
+    public boolean isBreedingItem(ItemStack stack) {
+        return stack.isOf(Items.APPLE);
+    }
+
+    public ActionResult interactMob(PlayerEntity player, Hand hand) {
+
+        var actionRseult = EntityUtils.tameOrHealHandle(this, player, hand, Items.GLOW_BERRIES, 3);
+
+        return (actionRseult == null) ? super.interactMob(player, hand) : actionRseult;
     }
 }
--- src/main/java/sweet/wilderness/huh/EntityUtils.java
+++ src/main/java/sweet/wilderness/huh/EntityUtils.java
@@ -1,10 +1,64 @@
 package sweet.wilderness.huh;
 
 import net.minecraft.entity.Entity;
+import net.minecraft.entity.passive.TameableEntity;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+import net.minecraft.util.ActionResult;
+import net.minecraft.util.Hand;
 import net.minecraft.world.World;
+import org.jetbrains.annotations.Nullable;
 
 public class EntityUtils {
+
     public static void RemoveEntity(World world, Entity entity){
         entity.discard();
     }
+
+    @Nullable
+    public static ActionResult tameOrHealHandle(TameableEntity ent, PlayerEntity player,
+                                                Hand hand, Item tamingItem, int tameBound) {
+        ItemStack itemStack = player.getStackInHand(hand);
+        Item item = itemStack.getItem();
+
+        if (ent.getWorld().isClient) {
+            boolean shouldConsume = ent.isOwner(player)
+                    || ent.isTamed()
+                    || itemStack.isOf(tamingItem)
+                    && !ent.isTamed();
+            return shouldConsume ? ActionResult.CONSUME : ActionResult.PASS;
+        }
+
+        // serverside
+
+        if (ent.isTamed()
+                && ent.isBreedingItem(itemStack)
+                && ent.getHealth() < ent.getMaxHealth())
+        {
+            if (!player.getAbilities().creativeMode)
+                itemStack.decrement(1);
+
+            ent.heal((float) item.getFoodComponent().getHunger());
+            return ActionResult.SUCCESS;
+
+        }
+        else if (itemStack.isOf(tamingItem)) {
+            if (!player.getAbilities().creativeMode)
+                itemStack.decrement(1);
+
+            if (ent.getWorld().random.nextInt(tameBound) == 0) {
+                ent.setOwner(player);
+                ent.getNavigation().stop();
+                ent.getWorld().sendEntityStatus(ent, (byte) 7);
+            }
+            else
+                ent.getWorld().sendEntityStatus(ent, (byte) 6);
+
+            return ActionResult.SUCCESS;
+        }
+
+        return null;
+    }
+
 }

0121f24d4e335b843c06722e06d4307932b6da19
Fireball_2000 <kerlan.jacquette@gmail.com>
2024-04-08 09:54:25 -0400

    capybara tower
    capybara tower

--- src/main/java/sweet/wilderness/entity/entities/capybara/CapybaraEntity.java
+++ src/main/java/sweet/wilderness/entity/entities/capybara/CapybaraEntity.java
@@ -1,6 +1,9 @@
 package sweet.wilderness.entity.entities.capybara;
 
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.EntityDimensions;
 import net.minecraft.entity.EntityType;
+import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.ai.goal.*;
 import net.minecraft.entity.attribute.DefaultAttributeContainer;
 import net.minecraft.entity.attribute.EntityAttributes;
@@ -8,14 +11,21 @@ import net.minecraft.entity.passive.AnimalEntity;
 import net.minecraft.entity.passive.PassiveEntity;
 import net.minecraft.entity.passive.TameableEntity;
 import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.fluid.FluidState;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.Items;
+import net.minecraft.predicate.entity.EntityPredicates;
+import net.minecraft.registry.tag.FluidTags;
 import net.minecraft.server.world.ServerWorld;
 import net.minecraft.util.ActionResult;
 import net.minecraft.util.Hand;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.Box;
+import net.minecraft.util.math.MathHelper;
 import net.minecraft.world.EntityView;
 import net.minecraft.world.World;
 import org.jetbrains.annotations.Nullable;
+import org.joml.Vector3f;
 import software.bernie.geckolib.animatable.GeoEntity;
 import software.bernie.geckolib.core.animatable.GeoAnimatable;
 import software.bernie.geckolib.core.animatable.instance.AnimatableInstanceCache;
@@ -25,10 +35,14 @@ import software.bernie.geckolib.util.GeckoLibUtil;
 import sweet.wilderness.entity.ModEntities;
 import sweet.wilderness.huh.EntityUtils;
 
+import java.util.Iterator;
+import java.util.List;
+
 public class CapybaraEntity extends TameableEntity implements GeoEntity {
     private AnimatableInstanceCache cache = GeckoLibUtil.createInstanceCache(this);
 
     public static final float MOVEMENT_SPEED = .5201f;
+    private float ticksUnderwater;
 
     public CapybaraEntity(EntityType<? extends AnimalEntity> entityType, World world) {
         super((EntityType<? extends TameableEntity>) entityType, world);
@@ -74,9 +88,47 @@ public class CapybaraEntity extends TameableEntity implements GeoEntity {
         return PlayState.CONTINUE;
     }
 
+    protected Vector3f getPassengerAttachmentPos(Entity passenger, EntityDimensions dimensions, float scaleFactor) {
+        return new Vector3f(0.0F, dimensions.height * 0.8888889F, 0.0f);
+    }
+
     @Override
     public void tick() {
+        if (!isUnderWater()) this.ticksUnderwater = 0.0F;
+        else ++this.ticksUnderwater;
+
+        if (!this.getWorld().isClient && this.ticksUnderwater >= 60.0F)
+            this.removeAllPassengers();
+
         super.tick();
+
+        this.checkBlockCollision();
+        List<Entity> list = this.getWorld().getOtherEntities(this, this.getBoundingBox().expand(0.20000000298023224, -0.009999999776482582, 0.20000000298023224), EntityPredicates.canBePushedBy(this));
+
+        if (list.isEmpty()) return;
+
+        Iterator var10 = list.iterator();
+
+        while (true) {
+            Entity entity;
+
+            do
+            {
+                if (!var10.hasNext()) return;
+
+                entity = (Entity) var10.next();
+            } while (entity.hasPassenger(this));
+
+            if (!this.getWorld().isClient && this.getPassengerList().isEmpty()
+                && !entity.hasVehicle() && entity instanceof LivingEntity
+                && !(entity instanceof PlayerEntity))
+            {
+                entity.startRiding(this);
+            } else this.pushAwayFrom(entity);
+
+        }
+
+
     }
 
     @Override
@@ -100,5 +152,34 @@ public class CapybaraEntity extends TameableEntity implements GeoEntity {
         var actionRseult = EntityUtils.tameOrHealHandle(this, player, hand, Items.GLOW_BERRIES, 3);
 
         return (actionRseult == null) ? super.interactMob(player, hand) : actionRseult;
+    }
+
+    private boolean isUnderWater() {
+        Box box = this.getBoundingBox();
+        double d = box.maxY + 0.001;
+        int i = MathHelper.floor(box.minX);
+        int j = MathHelper.ceil(box.maxX);
+        int k = MathHelper.floor(box.maxY);
+        int l = MathHelper.ceil(d);
+        int m = MathHelper.floor(box.minZ);
+        int n = MathHelper.ceil(box.maxZ);
+
+        BlockPos.Mutable mutable = new BlockPos.Mutable();
+
+        for(int o = i; o < j; ++o) {
+            for(int p = k; p < l; ++p) {
+                for(int q = m; q < n; ++q) {
+                    mutable.set(o, p, q);
+                    FluidState fluidState = this.getWorld().getFluidState(mutable);
+
+                    if (fluidState.isIn(FluidTags.WATER)
+                        && d < (double)((float)mutable.getY() + fluidState.getHeight(this.getWorld(), mutable)))
+                        return true;
+
+                }
+            }
+        }
+
+        return false;
     }
 }
